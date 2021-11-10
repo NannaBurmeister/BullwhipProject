1ebdfd73d934214d5ce4b22e1fd76648335e9bf2
NannaBurmeister <nnnburmeister@gmail.com>
2021-11-10 13:02:46 +0100

    Smarter code

--- src/OJN_simulation_manu.R
+++ src/OJN_simulation_manu.R
@@ -42,35 +42,16 @@ generate_leadtimes_manufacturer <- function(N, C, K, M, alpha, mu_I, mu_D1, mu_D
   
   arrival_D1 <- as.tibble(arrival_D1)
   
-  #Split into vectors
-  arrival_D1_1 <- arrival_D1 %>% filter(V3 == 1) %>% select(-V3) %>% as.matrix()
-  arrival_D1_2 <- arrival_D1 %>% filter(V3 == 2) %>% select(-V3) %>% as.matrix()
-  arrival_D1_3 <- arrival_D1 %>% filter(V3 == 3) %>% select(-V3) %>% as.matrix()
-  arrival_D1_4 <- arrival_D1 %>% filter(V3 == 4) %>% select(-V3) %>% as.matrix()
-  arrival_D1_5 <- arrival_D1 %>% filter(V3 == 5) %>% select(-V3) %>% as.matrix()
-  arrival_D1_6 <- arrival_D1 %>% filter(V3 == 6) %>% select(-V3) %>% as.matrix()
+  D1 <- map(1:6, function(k) {
+    arrival_node_k <- arrival_D1 %>% filter(V3 == k) %>% select(-V3) %>% as.matrix()
+    sim_output <- simulate_mm1_queue(arrival_node_k, mu_D1)
+    sim_output <- cbind(sim_output$id, sim_output$leave_service_time)
+    return(sim_output)
+  })
   
+  D1 <- do.call(rbind, D1)
   
-  #Now simulate M/M/1 queues at each node in department 1
-  D1_1 <- simulate_mm1_queue(arrival_D1_1, mu_D1)
-  D1_2 <- simulate_mm1_queue(arrival_D1_2, mu_D1)
-  D1_3 <- simulate_mm1_queue(arrival_D1_3, mu_D1)
-  D1_4 <- simulate_mm1_queue(arrival_D1_4, mu_D1)
-  D1_5 <- simulate_mm1_queue(arrival_D1_5, mu_D1)
-  D1_6 <- simulate_mm1_queue(arrival_D1_6, mu_D1)
-  
-  
-  ##Department 2
-  #Vector with id and interarrival for D2
-  D1_1 <- cbind(D1_1$id, D1_1$leave_service_time)
-  D1_2 <- cbind(D1_2$id, D1_2$leave_service_time)
-  D1_3 <- cbind(D1_3$id, D1_3$leave_service_time)
-  D1_4 <- cbind(D1_4$id, D1_4$leave_service_time)
-  D1_5 <- cbind(D1_5$id, D1_5$leave_service_time)
-  D1_6 <- cbind(D1_6$id, D1_6$leave_service_time)
-  
-  arrival_D2 <- rbind(D1_1, D1_2, D1_3, D1_4, D1_5, D1_6)
-  arrival_D2 <- arrival_D2[order(arrival_D2[,2]),]
+  arrival_D2 <- D1[order(D1[,2]),]
   arrival_D2 <- cbind(arrival_D2,rep(0,length(arrival_D2[,2])))
   
   for (i in 1:length(arrival_D2[,2])){
@@ -81,35 +62,16 @@ generate_leadtimes_manufacturer <- function(N, C, K, M, alpha, mu_I, mu_D1, mu_D
   
   arrival_D2 <- as.tibble(arrival_D2)
   
-  #Split into vectors
-  arrival_D2_1 <- arrival_D2 %>% filter(V3 == 1) %>% select(-V3) %>% as.matrix()
-  arrival_D2_2 <- arrival_D2 %>% filter(V3 == 2) %>% select(-V3) %>% as.matrix()
-  arrival_D2_3 <- arrival_D2 %>% filter(V3 == 3) %>% select(-V3) %>% as.matrix()
-  arrival_D2_4 <- arrival_D2 %>% filter(V3 == 4) %>% select(-V3) %>% as.matrix()
-  arrival_D2_5 <- arrival_D2 %>% filter(V3 == 5) %>% select(-V3) %>% as.matrix()
-  arrival_D2_6 <- arrival_D2 %>% filter(V3 == 6) %>% select(-V3) %>% as.matrix()
+  #Now simulate M/M/1 queues at each node in department 2
+  D2 <- map(1:6, function(k) {
+    arrival_node_k <- arrival_D2 %>% filter(V3 == k) %>% select(-V3) %>% as.matrix()
+    sim_output <- simulate_mm1_queue(arrival_node_k, mu_D2)
+    sim_output <- cbind(sim_output$id, sim_output$leave_service_time)
+    return(sim_output)
+  })
   
-  
-  #Now simulate M/M/1 queues at each node in department 1
-  D2_1 <- simulate_mm1_queue(arrival_D2_1, mu_D2)
-  D2_2 <- simulate_mm1_queue(arrival_D2_2, mu_D2)
-  D2_3 <- simulate_mm1_queue(arrival_D2_3, mu_D2)
-  D2_4 <- simulate_mm1_queue(arrival_D2_4, mu_D2)
-  D2_5 <- simulate_mm1_queue(arrival_D2_5, mu_D2)
-  D2_6 <- simulate_mm1_queue(arrival_D2_6, mu_D2)
-  
-  
-  ##Department 3
-  #Vector with id and interarrival for D3
-  D2_1 <- cbind(D2_1$id, D2_1$leave_service_time)
-  D2_2 <- cbind(D2_2$id, D2_2$leave_service_time)
-  D2_3 <- cbind(D2_3$id, D2_3$leave_service_time)
-  D2_4 <- cbind(D2_4$id, D2_4$leave_service_time)
-  D2_5 <- cbind(D2_5$id, D2_5$leave_service_time)
-  D2_6 <- cbind(D2_6$id, D2_6$leave_service_time)
-  
-  arrival_D3 <- rbind(D2_1, D2_2, D2_3, D2_4, D2_5, D2_6)
-  arrival_D3 <- arrival_D3[order(arrival_D3[,2]),]
+  D2 <- do.call(rbind, D2)
+  arrival_D3 <- D2[order(D2[,2]),]
   arrival_D3 <- cbind(arrival_D3,rep(0,length(arrival_D3[,2])))
   
   for (i in 1:length(arrival_D3[,2])){
@@ -120,34 +82,16 @@ generate_leadtimes_manufacturer <- function(N, C, K, M, alpha, mu_I, mu_D1, mu_D
   
   arrival_D3 <- as.tibble(arrival_D3)
   
-  #Split into vectors
-  arrival_D3_1 <- arrival_D3 %>% filter(V3 == 1) %>% select(-V3) %>% as.matrix()
-  arrival_D3_2 <- arrival_D3 %>% filter(V3 == 2) %>% select(-V3) %>% as.matrix()
-  arrival_D3_3 <- arrival_D3 %>% filter(V3 == 3) %>% select(-V3) %>% as.matrix()
-  arrival_D3_4 <- arrival_D3 %>% filter(V3 == 4) %>% select(-V3) %>% as.matrix()
-  arrival_D3_5 <- arrival_D3 %>% filter(V3 == 5) %>% select(-V3) %>% as.matrix()
-  arrival_D3_6 <- arrival_D3 %>% filter(V3 == 6) %>% select(-V3) %>% as.matrix()
+  #Now simulate M/M/1 queues at each node in department 2
+  D3 <- map(1:6, function(k) {
+    arrival_node_k <- arrival_D3 %>% filter(V3 == k) %>% select(-V3) %>% as.matrix()
+    sim_output <- simulate_mm1_queue(arrival_node_k, mu_D3)
+    sim_output <- cbind(sim_output$id, sim_output$leave_service_time)
+    return(sim_output)
+  })
   
-  
-  #Now simulate M/M/1 queues at each node in department 1
-  D3_1 <- simulate_mm1_queue(arrival_D3_1, mu_D3)
-  D3_2 <- simulate_mm1_queue(arrival_D3_2, mu_D3)
-  D3_3 <- simulate_mm1_queue(arrival_D3_3, mu_D3)
-  D3_4 <- simulate_mm1_queue(arrival_D3_4, mu_D3)
-  D3_5 <- simulate_mm1_queue(arrival_D3_5, mu_D3)
-  D3_6 <- simulate_mm1_queue(arrival_D3_6, mu_D3)
-  
-  
-  ##Output node node
-  #Vector with id and interarrival for output node
-  D3_1 <- cbind(D3_1$id, D3_1$leave_service_time)
-  D3_2 <- cbind(D3_2$id, D3_2$leave_service_time)
-  D3_3 <- cbind(D3_3$id, D3_3$leave_service_time)
-  D3_4 <- cbind(D3_4$id, D3_4$leave_service_time)
-  D3_5 <- cbind(D3_5$id, D3_5$leave_service_time)
-  D3_6 <- cbind(D3_6$id, D3_6$leave_service_time)
-  
-  arrival_O <- rbind(D3_1, D3_2, D3_3, D3_4, D3_5, D3_6, arrival_O_temp)
+  D3 <- do.call(rbind, D3)
+  arrival_O <- rbind(D3, arrival_O_temp)
   arrival_O <- arrival_O[order(arrival_O[,2]),]
   
   #Simulate
